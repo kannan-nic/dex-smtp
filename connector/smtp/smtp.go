cc4313939578b42ac84bc4801a28274766a2d112
kannan-nic <168638714+kannan-nic@users.noreply.github.com>
2024-05-02 10:56:09 +0530

    Update smtp.go
    Signed-off-by: kannan-nic <168638714+kannan-nic@users.noreply.github.com>

--- connector/smtp/smtp.go
+++ connector/smtp/smtp.go
@@ -38,18 +38,7 @@ func (sc *smtpConnector) Login(ctx context.Context, _ connector.Scopes, username
 
 	var conn net.Conn
 	
-    if strings.Contains(p, "tls") {		
-		sc.cfg.Host = sc.cfg.Host[0 : len(sc.cfg.Host)-3]
-		p = p[0 : len(p)-3]
-		tlsconfig := &tls.Config {
-        	InsecureSkipVerify: true,
-        	ServerName: h,
-    	}	 
-		conn, err = tls.Dial("tcp", sc.cfg.Host, tlsconfig)
-		if err != nil {
-			return
-		}
-	} else if p == "" || p == "465" {
+    if p == "" || p == "465" {
 		conn, err = tls.Dial("tcp", sc.cfg.Host, nil)
 		if err != nil {
 			return
